1c16fa41385b4211eb0ea28f70d334fa7997cc6a
Arthur Rocha de Menezes <arthurstomp@gmail.com>
2013-12-15 00:51:05 -0300

    add function of powerset(conjunto das partes) got it on the internet.

--- afd.hs
+++ afd.hs
@@ -2,13 +2,21 @@
 
 import Data.Set
 import Data.Matrix
+import Prelude hiding (map)
 
 data AFD a = AFD { states :: Set a
-               , alphabet :: Set a
-               , transition :: Matrix a
-               , initialState :: Integer
-               , finalStates :: Set a
-               } deriving (Show)
+                 , alphabet :: Set a
+                 , transition :: Matrix a
+                 , initialState :: Integer
+                 , finalStates :: Set a
+                 } deriving (Show)
 
 test :: AFD a 
 test = AFD {states = Data.Set.empty, alphabet = Data.Set.empty, transition = Data.Matrix.fromList 0 0 [], initialState = 0, finalStates = Data.Set.empty}
+
+powerset s
+    | s == empty = singleton empty
+    | otherwise = map (insert x) pxs `union` pxs
+      where (x, xs) = deleteFindMin s
+            pxs = powerset xs
+
