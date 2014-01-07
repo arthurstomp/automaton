d1c87c2354b9fb31960548a587f26498c365a2d0
Arthur Rocha de Menezes <arthurstomp@gmail.com>
2014-01-07 15:37:22 -0300

    Change the way how AFN is done.

--- afn.hs
+++ afn.hs
@@ -1,84 +1,95 @@
 --Functions for AFN
 
-module AFN
-( afn
-, AFN
-, compute
-, afn1
-, states
-, alphabet
-, initialState
-, finalStates
-, transition
-)where
+--module AFN
+--( afn
+--, AFN
+--, compute
+--, afn1
+--, states
+--, alphabet
+--, initialState
+--, finalStates
+--, transition
+--)where
 
 import Data.Set
 import Data.Matrix
 import Data.List
 import Data.Maybe
 
-data AFN = AFN { states :: Set Int
-               , alphabet :: Set Char
-               , transition :: Matrix (Maybe (Set Int))
+data AFN = AFN { states :: [Int]
+               , alphabet :: [Char]
+               , transition :: Matrix (Set Int)
                , initialState :: Int
-               , finalStates :: Set Int
+               , finalStates :: [Int]
                } deriving (Show)
 
-afn :: [Int] -> [Char] -> ((Int,Int) -> Maybe (Set Int)) -> Int -> [Int] -> AFN
-afn s a ft is fs = AFN { states = Data.Set.fromList s
+afn :: [Int] -> [Char] -> Matrix (Set Int) -> Int -> [Int] -> AFN
+afn s a t is fs = AFN { states = s
                        , alphabet = alpha
                        , transition = t
                        , initialState = is
-                       , finalStates = (Data.Set.fromList fs)
+                       , finalStates = fs
                        }
-  where t = Data.Matrix.matrix (length s) (Data.Set.size alpha) $ ft
-        alpha = Data.Set.insert 'E' $ Data.Set.fromList a
+  where alpha = Data.List.insert 'E' a
 
--- Durante a passagem do estado eu preciso lembrar de não salvar estados Nothing
-accept :: AFN -> Set (Maybe Int) -> Bool
+accept :: AFN -> Set Int -> Bool
 accept afn cs  = True `elem` acceptance 
-  where acceptance = [ Data.Set.member (Data.Maybe.fromJust x) $ finalStates afn | x <- Data.Set.elems cs] 
+  where acceptance = [ Data.List.elem x $ finalStates afn | x <- Data.Set.elems cs] 
 
-compute :: AFN -> Set (Maybe Int) -> [Char] -> Bool
+compute :: AFN -> Set Int -> [Char] -> Bool
 compute afn cs [] = accept afn cs
 compute afn cs (x:xs) = compute afn ns xs
   where ns = nextState afn cs x
 
-prettyInitialState :: AFN -> Set (Maybe Int)
-prettyInitialState afn = Data.Set.singleton $ Just $ initialState afn
-
-nextState :: AFN -> Set (Maybe Int) -> Char -> Set (Maybe Int)
+nextState :: AFN -> Set Int -> Char -> Set Int
 nextState afn cs t = Data.Set.union (partialNextState afn cs t) (partialNextState afn cs 'E')
 
-partialNextState :: AFN -> Set (Maybe Int) -> Char -> Set (Maybe Int)
-partialNextState afn cs t = Data.Set.unions [transitionFromState afn (Data.Maybe.fromJust x) t | x <- Data.Set.elems cs]
+partialNextState :: AFN -> Set Int -> Char -> Set Int
+partialNextState afn cs c = Data.Set.unions [transitionFromState afn x c | x <- Data.Set.elems cs]
 
-transitionFromState :: AFN -> Int -> Char -> Set (Maybe Int)
-transitionFromState afn cs t = case ns of
-  Just n -> Data.Set.map Just n
-  Nothing -> Data.Set.empty
+prettyInitialState :: AFN -> Set Int
+prettyInitialState afn = Data.Set.singleton $ initialState afn
+
+transitionFromState :: AFN -> Int -> Char -> Set Int
+transitionFromState afn cs t
+  | ns == Data.Set.empty = Data.Set.singleton cs 
+  | ns /= Data.Set.empty = ns
   where it = indexTransition afn t
         ns = (transition afn) Data.Matrix.! (cs,it+1)
 
-
 indexTransition :: AFN -> Char -> Int
-indexTransition afn t = Data.Maybe.fromJust $ Data.List.elemIndex t ascList
+indexTransition afn t = Data.Maybe.fromJust $ Data.List.elemIndex t a
   where a = alphabet afn
-        ascList = Data.Set.toAscList a 
 
-foo :: (Int,Int) -> Maybe (Set Int)
-foo (x,y)
-  | x == 1 && y == 1 = Just $ Data.Set.singleton 1
-  | x == 1 && y == 2 = Just $ Data.Set.fromList [1,2]
-  | x == 1 && y == 3 = Nothing
-  | x == 2 && y == 1 = Just $ Data.Set.singleton 3
-  | x == 2 && y == 2 = Nothing
-  | x == 2 && y == 3 = Just $ Data.Set.singleton 3
-  | x == 3 && y == 1 = Nothing
-  | x == 3 && y == 2 = Just $ Data.Set.singleton 4
-  | x == 3 && y == 3 = Nothing
-  | x == 4 && y == 1 = Just $ Data.Set.singleton 4
-  | x == 4 && y == 2 = Just $ Data.Set.singleton 4
-  | x == 4 && y == 3 = Nothing
+-- Testing Area
+mountMatrix :: [Int] -> [Char] -> ((Int,Char) -> Set Int) -> Matrix (Set Int)
+mountMatrix s a foo = Data.Matrix.fromLists [possibleTransitions x a foo | x <- s]
 
-afn1 = afn [1,2,3,4] ['0','1'] foo 1 [4] 
+possibleTransitions :: Int -> [Char] -> ((Int,Char) -> Set Int) -> [Set Int]
+possibleTransitions cs a foo = [foo(cs,c) | c <- a]
+
+foo :: (Int,Char) -> Set Int
+foo (cs,c)
+  | cs == 1 && c == '0' = Data.Set.fromList [1,2] 
+  | cs == 1 && c == '1' = Data.Set.singleton 1
+  | cs == 1 && c == 'E' = Data.Set.empty
+  | cs == 2 && c == '0' = Data.Set.singleton 3
+  | cs == 2 && c == '1' = Data.Set.empty
+  | cs == 2 && c == 'E' = Data.Set.empty
+  | cs == 3 && c == '0' = Data.Set.empty
+  | cs == 3 && c == '1' = Data.Set.empty
+  | cs == 3 && c == 'E' = Data.Set.empty
+
+afn1 = afn s a m is fs
+  where s = [1,2,3]
+        a = ['0','1']
+        m = mountMatrix s alpha foo
+        is = 1
+        fs = [3]
+        alpha = Data.List.insert 'E' a
+
+is = prettyInitialState afn1
+ns1 = nextState afn1 is '0'
+ns2 = nextState afn1 ns1 '0'
+ns3 = nextState afn1 ns2 '1'
